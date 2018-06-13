fb2959e7f56150f8f66d1c988de2c3d30e58420a
Kael <piskael03@gmail.com>
2018-06-13 01:31:34 -0400

    Login, Logout and Navbars

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import React, {Component} from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './store';
+import jwtDecode from 'jwt-decode';
+
+import setAuthToken from './utils/setAuthToken';
+import {logoutUser, setCurrentUser} from './actions/authAction';
 
 import Navbar from './components/layouts/Navbar';
 import Footer from './components/layouts/Footer';
@@ -9,20 +15,36 @@ import Register from './components/auths/Register';
 
 import './App.css';
 
+
+if (localStorage.jwt) {
+    setAuthToken(localStorage.jwt);
+    const decodedToken = jwtDecode(localStorage.jwt);
+    store.dispatch(setCurrentUser(decodedToken));
+
+    const currentTime = Date.now() / 1000;
+    if (decodedToken.exp < currentTime) {
+        store.dispatch(logoutUser());
+        window.location.href = '/login';
+    }
+
+}
+
 class App extends Component {
     render() {
         return (
-            <BrowserRouter>
-                <div>
-                    <Navbar/>
-                    <Route exact path={'/'} component={Landing}/>
-                    <div className="container">
-                        <Route exact path={'/register'} component={Register}/>
-                        <Route exact path={'/login'} component={Login}/>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <div>
+                        <Navbar/>
+                        <Route exact path={'/'} component={Landing}/>
+                        <div className='container'>
+                            <Route exact path={'/register'} component={Register}/>
+                            <Route exact path={'/login'} component={Login}/>
+                        </div>
+                        <Footer/>
                     </div>
-                    <Footer/>
-                </div>
-            </BrowserRouter>
+                </BrowserRouter>
+            </Provider>
         );
     }
 }
